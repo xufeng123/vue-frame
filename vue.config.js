76f5b171321c4f510149995b927f4f23d985df0e
徐峰 <xufeng1@syswin.com>
2019-04-04 11:47:52 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,6 @@ function resolve (dir) {
     return path.join(__dirname, '..', dir);
 }
 
-var devServer = {
-    port: 8088,
-    open: true
-};
-
 module.exports = {
     publicPath: '/',
     // 输出文件目录
@@ -66,11 +61,10 @@ module.exports = {
             ]
         };
     },
-    devServer: devServer
-    // devServer: {
-        // port: 8088,     // 端口
+    devServer: {
+        port: 8088,     // 端口
         // host: '',    // 不配置默认为本地的ip
-        // open: true,     // 配置自动打开浏览器
+        open: true,     // 配置自动打开浏览器
         // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
         // 配置多个代理
         // proxy: {
@@ -83,5 +77,5 @@ module.exports = {
         //         }
         //     }
         // }
-    // }
+    }
 };
