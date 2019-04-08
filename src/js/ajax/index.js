91c3efba4b1bc2b91121b42d0ec126988488dcc1
徐峰 <xufeng1@syswin.com>
2019-04-08 18:13:16 +0800

    增加响应超时的配置

--- src/js/ajax/index.js
+++ src/js/ajax/index.js
@@ -9,8 +9,7 @@ if (typeof Promise === 'undefined') {
     var Promise = _promise;
 }
 // 增加接口超时响应
-var instance = axios.create();
-instance.defaults.timeout = 2500;
+var instance = 10000;
 
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
@@ -40,7 +39,9 @@ export function post (uri, params, opts) {
     }
     let options = opts && opts.options || {}; // options of vue-resource
     options.params = opts && opts.query || {}; // 传入的参数，转换到query方式，具体说明查看API
-    return axios.post(uri, params).then(function (res) {
+    return axios.post(uri, params, {
+        timeout: instance
+    }).then(function (res) {
         if (opts && opts.loading) {
             store.dispatch('CLOSE_LOADING'); // 隐藏菊花
         }
@@ -62,7 +63,9 @@ export function get (uri, params, opts) {
     if (opts && opts.loading) {
         store.commit('OPEN_LOADING'); // 显示菊花
     }
-    return axios.get(uri, {params}).then(function (res) {
+    return axios.get(uri, {params}, {
+        timeout: instance
+    }).then(function (res) {
         if (opts && opts.loading) {
             store.dispatch('CLOSE_LOADING'); // 隐藏菊花
         }
